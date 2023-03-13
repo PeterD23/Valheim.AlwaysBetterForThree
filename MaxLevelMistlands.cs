2273a1904f1d34ddb3618fb23ef07c6bb0b11ec0
Alien <peterdavidson.23.94@gmail.com>
2023-03-13 20:47:57 +0000

    Initial commit

---
+++ MaxLevelMistlands.cs
@@ -0,0 +1,34 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace MaxLevelMistlands
+{
+    [BepInPlugin("MaxLevelMistlands", "Max Level Mistlands", "1.0.0")]
+    [BepInProcess("valheim.exe")]
+    public class ValheimMod : BaseUnityPlugin
+    {
+        private readonly Harmony harmony = new Harmony("MaxLevelMistlands");
+        private static ManualLogSource Logs;
+
+        private void Awake()
+        {
+            harmony.PatchAll();
+            Logs = Logger;
+            Logs.LogInfo("Successfully patched Galdr Table/Black Forge workaround!");
+        }
+
+        [HarmonyPatch(typeof(CraftingStation), nameof(CraftingStation.GetLevel))]
+        class Mistlands_Patch
+        {
+            [HarmonyPostfix]
+            static void Patch(ref int __result, ref string ___m_name)
+            {
+                if (___m_name.ToLower().Equals("$piece_magetable") || ___m_name.ToLower().Equals("$piece_blackforge"))
+                {
+                    __result = __result + 2;
+                }
+            }
+        }
+    }
+}
